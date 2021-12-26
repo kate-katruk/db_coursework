bbb5f6fca2579bb1baa3f9cb0f3118cdac725406
Kate Katruk <katkatruk@gmail.com>
2021-12-26 14:30:02 +0200

    done

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config():
+    dbname = "coursework"
+    user = "postgres"
+    password = "1"
+    host = "localhost:5432"
---
+++ views/graphsView.py
@@ -0,0 +1,17 @@
+from CUI.constructor import CUI
+from visualization.graphs import getTop15Categories,\
+    getManufactureDateStat,\
+    getTransactionDateStat,\
+    getTotalMoneyPerYear
+
+
+class GraphsView(object):
+    def __init__(self):
+        self.CUI = CUI('Graphs menu')
+        self.CUI.addField('Top 15 categories', lambda: getTop15Categories())
+        self.CUI.addField('Products per year statistic', lambda: getManufactureDateStat())
+        self.CUI.addField('Sale statistics', lambda: getTransactionDateStat())
+        self.CUI.addField('Get total money per year', lambda: getTotalMoneyPerYear())
+
+    def run(self):
+        self.CUI.run()
